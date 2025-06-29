0f1606823f5bd51db3ba73bbae5cfffac9d43c18
Afnan Abo-Asal <afnanalaa49@gmail.com>
2025-06-29 22:50:52 +0300

    categories read

---
+++ FakeStore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using FakeStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
